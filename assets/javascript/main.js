caaacad44048600abe08b15cb8936f23d21e2d84
BeaQ17 <beaq235@gmail.com>
2021-04-27 17:03:24 +0200

    js done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,24 +1,17 @@
-//Stampa numeri da 1 a 100
-var nums = [];
-for (var i = 0; i < 100; i++){
-    nums[i] = i + 1;
-    console.log(nums);
-}
+//Stampa numeri da 1 a 100, per i multipli di 3 stampa Fizz, per i multipli di 5 stampa Buzz, per i multipli di 3 e 5 stampa FizzBuzz
+for (var i = 1; i < 101; i++){
 
-//per i multipli di 3 stampa Fizz
-if (i % 3 == 0) {
-    
-}
+    if (i % 3 == 0 && i % 5 == 0){
+        document.getElementById("nums").innerHTML += "Fizzbuzz <br>";
+    } else if (i % 3 == 0){
+        document.getElementById("nums").innerHTML += "Fizz <br>";
+    } else if (i % 5 == 0) {
+        document.getElementById("nums").innerHTML += "Buzz <br>";
+    } else {
+        document.getElementById("nums").innerHTML += i + "<br>";
 
-//per i multipli di 5 stampa Buzz
-if (i % 5 == 0){
-
-}
-
-//per i multipli di 3 e 5 stampa FizzBuzz
-if (i % 3 == 0 && i % 5 == 0){
+    }
 
 }
-
 
 
