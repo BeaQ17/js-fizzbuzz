a529d3fe6cb119fbfc764f21922977c6d3f41869
BeaQ17 <beaq235@gmail.com>
2021-04-27 16:17:49 +0200

    initial commit + array

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+//Stampa numeri da 1 a 100
+var nums = [];
+for (var i = 0; i < 100; i++){
+    nums[i] = i + 1;
+    console.log(nums);
+}
+
+//per i multipli di 3 stampa Fizz
+
+//per i multipli di 5 stampa Buzz
+//per i multipli di 3 e 5 stampa FizzBuzz
+
