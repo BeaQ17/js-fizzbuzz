dde523771d451f0e09378fb2573ef078758c737f
BeaQ17 <beaq235@gmail.com>
2021-04-27 16:30:22 +0200

    fizz and buzz non pervenuti

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,7 +6,19 @@ for (var i = 0; i < 100; i++){
 }
 
 //per i multipli di 3 stampa Fizz
+if (i % 3 == 0) {
+    
+}
 
 //per i multipli di 5 stampa Buzz
+if (i % 5 == 0){
+
+}
+
 //per i multipli di 3 e 5 stampa FizzBuzz
+if (i % 3 == 0 && i % 5 == 0){
+
+}
+
+
 
